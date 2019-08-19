8d95933c6e673aa3e1ef6dc3a9a1d445c8d7b0b0
zhangting9756 <tingzhangguo@163.com>
2019-08-20 00:46:59 +0800

    添加表

---
+++ MySql/练习表/course.sql
@@ -0,0 +1,4 @@
+INSERT INTO `course` (`cid`, `name`) VALUES (1, 'java');
+INSERT INTO `course` (`cid`, `name`) VALUES (2, 'html');
+INSERT INTO `course` (`cid`, `name`) VALUES (3, '英语');
+INSERT INTO `course` (`cid`, `name`) VALUES (4, '数学');
---
+++ MySql/练习表/stu.sql
@@ -0,0 +1,5 @@
+INSERT INTO `stu` VALUES (1001, '张三', 20, '上海', '男');
+INSERT INTO `stu` VALUES (1002, '李四', 22, '北京', '男');
+INSERT INTO `stu` VALUES (1003, '王五', 20, '上海', '女');
+INSERT INTO `stu` VALUES (1004, '赵六', 18, '北京', '男');
+INSERT INTO `stu` VALUES (1005, '周七', 22, '北京', '男');
